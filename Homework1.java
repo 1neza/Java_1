024a77aa69dd893f7859f24b56e6ef0b86f5ede0
1neza <inezajean22@gmail.com>
2024-02-25 16:23:07 +0100

    Second Commit after Switching branch forgotten semi-colon before!

---
+++ Homework1.java
@@ -0,0 +1,17 @@
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+         //first commit after creating repo and hello world! Java
+
+        System.out.println(" ");
+        System.out.println("Hello World!"); 
+        System.out.println(" ");
+
+        //Second Commit by adding other prt and swiching branch
+        System.out.println("switching branches and second commit");
+        System.out.println("Yo! Checkout my sbt. Code: 66539");
+        System.out.println(" ");
+        
+    }
+}

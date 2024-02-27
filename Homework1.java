49bec133dba6c2d880d045af3cce01edf5d6712a
1neza <inezajean22@gmail.com>
2024-02-27 23:40:38 +0100

    learning commit style now am using commit -a

--- Homework1.java
+++ Homework1.java
@@ -10,14 +10,15 @@ public class Homework1 {
         //Second Commit by adding other part, learning cmd prompt (vim) and swiching branch
 	
         System.out.println("switching branches and second commit");
-        System.out.println("Yo! Checkout my sbt. Code: 66539");
+        System.out.println(" ");
+	System.out.println("Yo! Checkout my sbt. Code: 66539");
         System.out.println(" ");
         
 	//Day with Bash 02.27
 	System.out.println(" ");
 	System.out.println("Today am playing with bash commands navigating, file ops and bash visual cmds! ");
-	System.out.println("");
+	System.out.println(" ");
     	System.out.println("Reaady to commit 12.01 all java codes writen in Bash no test no jokes <-->");
-	System.out.println("");
+	System.out.println(" ");
     }
 }

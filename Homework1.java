72ba7979f74e306a72c22300a66fce5a182a8845
1neza <inezajean22@gmail.com>
2024-02-27 12:06:38 +0100

    Day 2 with Bash No GUis

--- Homework1.java
+++ Homework1.java
@@ -3,15 +3,21 @@ public class Homework1 {
     public static void main(String[] args) {
 
          //first commit after creating repo and hello world! Java
-
         System.out.println(" ");
         System.out.println("Hello World!"); 
         System.out.println(" ");
 
-        //Second Commit by adding other prt and swiching branch
+        //Second Commit by adding other part, learning cmd prompt (vim) and swiching branch
+	
         System.out.println("switching branches and second commit");
         System.out.println("Yo! Checkout my sbt. Code: 66539");
         System.out.println(" ");
         
+	//Day with Bash 02.27
+	System.out.println(" ");
+	System.out.println("Today am playing with bash commands navigating, file ops and bash visual cmds! ");
+	Sytem.out.println("");
+    	Sytem.out.println("Reaady to commit 12.01 all java codes writen in Java no test no jokes <-->");
+	Sytem.out.println("");
     }
 }
